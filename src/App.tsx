144bad4ae7ac4e34c694b31a3ca5a8806fc1b76b
Glody Bulambo <bgloire@andrew.cmu.edu>
2025-07-23 16:44:22 +0200

    get started

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className='text-4xl font-bold text-center'>
+      VUTUKA EDITION
+    </div>
+  )
+}
+
+export default App
